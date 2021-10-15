33f95284ea2e9266b6973bf6d70f088b1b35f12b
Sandrine-a <sandrine_a971@yahoo.fr>
2021-10-14 21:08:19 -0400

    Modification models USER BDD

--- back-end/models/Post.js
+++ back-end/models/Post.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Post.init({
-    idUser: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     attachment: DataTypes.STRING,
